2df244ecc0ba1e3df79ef7278733e23182068c2e
Rajaswi Borkar <64773403+RajaswiBorkar15@users.noreply.github.com>
2024-09-11 13:59:42 +0530

    Create Yunlis subqueries(Easy version).cpp

---
+++ Yunlis subqueries(Easy version).cpp
@@ -0,0 +1,37 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int checksubarray(int arr[],int k , int m,int l){
+    int cnt =0;
+    unordered_set<int> st;
+    for(int i=k;i<m;i++){
+        st.insert(arr[i]);
+    }
+    for(int i =1;i<st.size();i++){
+        if(arr[i] > arr[i-1]){
+            
+        }else{
+            cnt++;
+        }
+    }
+    
+    return cnt;
+}
+int main() {
+    // Write C++ code here
+    int n , s ;//,c; // n = array length, s = sub-array length, c = no of times subaaray would be made 
+    cin>>n; 
+    cin>>s; //<< c;
+    int arr[n];
+    for(int i =1;i<=n;i++){
+        cin>>arr[i];
+    }
+    int start , end;
+
+        cin>>start;
+        cin>>end;
+    int cnt = checksubarray(arr,start,end,s);
+    cout<<cnt;
+    return 0;
+}
